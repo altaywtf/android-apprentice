a1bb45c486b2b3138c6071d069a016be4244b127
Altay Aydemir <altay@aydemir.io>
2021-07-23 10:15:34 +0200

    use search intent to perform search

--- PodPlay/app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
+++ PodPlay/app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
@@ -2,6 +2,7 @@ package com.raywenderlich.podplay.ui
 
 import android.app.SearchManager
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -19,13 +20,6 @@ class PodcastActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podcast)
-
-        val itunesRepo = ItunesRepo(ItunesService.instance)
-
-        GlobalScope.launch {
-            val results = itunesRepo.searchByTerm("Android Developer")
-            Log.i(TAG, "Results = ${results.body()}")
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -39,5 +33,29 @@ class PodcastActivity : AppCompatActivity() {
         searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
 
         return true
+    }
+
+    private fun performSearch(term: String) {
+        val itunesRepo = ItunesRepo(ItunesService.instance)
+
+        GlobalScope.launch {
+            val results = itunesRepo.searchByTerm(term)
+            Log.i(TAG, "Results = ${results.body()}")
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        when (intent.action) {
+            Intent.ACTION_SEARCH -> {
+                val query = intent.getStringExtra(SearchManager.QUERY) ?: return
+                performSearch(query)
+            }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
     }
 }
