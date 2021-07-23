bdf593e6f0c9119ec233fa5bd8ff93879e893de8
Altay Aydemir <altay@aydemir.io>
2021-07-23 09:20:18 +0200

    init podplay app

---
+++ PodPlay/app/src/main/java/com/raywenderlich/podplay/PodcastActivity.kt
@@ -0,0 +1,11 @@
+package com.raywenderlich.podplay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PodcastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_podcast)
+    }
+}
