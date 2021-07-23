fd802f3dd95869834f45899242a72d06937b979d
Altay Aydemir <altay@aydemir.io>
2021-07-23 09:36:42 +0200

    add retrofit for networking

--- PodPlay/app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
+++ PodPlay/app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
@@ -1,7 +1,8 @@
-package com.raywenderlich.podplay
+package com.raywenderlich.podplay.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.raywenderlich.podplay.R
 
 class PodcastActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
